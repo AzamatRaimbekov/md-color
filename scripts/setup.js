82390ecfa89f70a3b190cd116540b0d776d225fc
Азамат Раимбеков Кололь фронта <arturfeniks88@gmail.com>
2025-02-11 09:32:54 -0800

    check

--- scripts/setup.js
+++ scripts/setup.js
@@ -8,10 +8,12 @@ const theme = process.env.npm_config_theme || 'default'
 const source = path.join(__dirname, '..', 'brands', `${theme}.json`)
 const dest = path.join(__dirname, '..', 'brand.json')
 
+// Проверяем, существует ли файл с темой
 if (fs.existsSync(source)) {
-	fs.copyFileSync(source, dest)
+	fs.copyFileSync(source, dest) // Синхронное копирование файла
 	console.log(`✅ Установлена тема: ${theme}`)
 } else {
+	// Если файл с темой не найден, используем default.json
 	console.error(`⚠️ Тема ${theme}.json не найдена! Устанавливаем default.json`)
 	fs.copyFileSync(path.join(__dirname, '..', 'palettes', 'default.json'), dest)
 }
